f9945f44cb918ec407ad2190214a57a57e851b67
Yukyan <yukyanjpn@gmail.com>
2021-10-28 23:55:34 +0900

    Broken Authenticationにしたいので

--- psemi2021_app_a/settings.py
+++ psemi2021_app_a/settings.py
@@ -117,6 +117,8 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
+AUTH_PASSWORD_VALIDATORS = []
+"""
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -131,6 +133,7 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+"""
 
 
 # Internationalization
